8ac8735cee800d5b73da2c574ae34bfc2006e36d
DulanjanaLakshan <dulanjana20013@gmail.com>
2026-03-03 15:00:06 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({mode}) => {
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+    preview: {
+      host: true,
+      port: 3000,
+      allowedHosts: ["nu3go.cypsolabs.com","www.nu3go.cypsolabs.com"],
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
